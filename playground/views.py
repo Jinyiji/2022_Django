0589bca8c02f6e475de3fac21312fee22a222eeb
Jinyiji <w2018@e-mirim.hs.kr>
2022-03-21 14:50:31 +0900

    views: {'name': '이름'} -> templates: {{ name }}

--- playground/views.py
+++ playground/views.py
@@ -7,7 +7,7 @@ def say_hello(request):
 
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html', {'name': '전정국'})
 
 
 def say_bye_html(request):
